796b465c5e2c93d6b34830d71c49db0ce31dd19e
MicroDuke <fiber@foxmail.com>
2022-08-22 15:24:48 +0800

    Update txt2stock.py
    Add Total B/S

--- txt2stock.py
+++ txt2stock.py
@@ -50,11 +50,12 @@ def wb_init ():
 
     global NUMBER,ROW,COLUMN,DIR,OFILE,wb,ws;
      
-    #COLUMN=['日期'	'买交易量B1'	'卖交易量S1'	'......' '买交易量Bn'	'卖交易量Sn'	'BM1'	'SM1' '......'	'BMn'	'SMn']
+    #COLUMN=['日期'	'买交易量B1'	'卖交易量S1'	'......' 'Total''买交易量Bn'	'卖交易量Sn'	'BM1'	'SM1' '......'	'BMn'	'SMn']
     COLUMN=[]
     COLUMN.insert(0,'文件名')
     COLUMN.insert(2*len(NUMBER)+1 , 'B[>' + str(NUMBER[-1]) + ']')
     COLUMN.insert(2*len(NUMBER)+2 , 'S[>' + str(NUMBER[-1]) + ']')
+    COLUMN.insert(2*len(NUMBER)+3 , 'Total') # ADD Total
     #print(COLUMN)
     #print('NUMBER=',NUMBER)
 
@@ -66,16 +67,16 @@ def wb_init ():
         # print('id=',id)
         COLUMN.insert(2*id+1,'B[' + str(NUMBER[id]) + ']')
         COLUMN.insert(2*id+2,'S[' + str(NUMBER[id]) + ']')
-        COLUMN.insert(2*len(NUMBER)+2*id+3 , 'BM[' + str(NUMBER[id]) + ']')
-        COLUMN.insert(2*len(NUMBER)+2*id+4 , 'SM[' + str(NUMBER[id]) + ']')
+        COLUMN.insert(2*len(NUMBER)+2*id+3+1 , 'BM[' + str(NUMBER[id]) + ']')
+        COLUMN.insert(2*len(NUMBER)+2*id+4+1 , 'SM[' + str(NUMBER[id]) + ']')
 
 
         
         # print(2*id+1,2*id+2,2*len(NUMBER)+2*id+3,2*len(NUMBER)+2*id+4,4*len(NUMBER)+3,4*len(NUMBER)+4)
         # print(COLUMN,len(COLUMN))
 
-    COLUMN.insert(4*len(NUMBER)+3 , 'BM[>' + str(NUMBER[-1]) + ']')
-    COLUMN.insert(4*len(NUMBER)+4 , 'SM[>' + str(NUMBER[-1]) + ']')
+    COLUMN.insert(4*len(NUMBER)+3+1 , 'BM[>' + str(NUMBER[-1]) + ']')
+    COLUMN.insert(4*len(NUMBER)+4+1 , 'SM[>' + str(NUMBER[-1]) + ']')
 
     for i in range(0,len(COLUMN)):
         ws.write(0,i,COLUMN[i]) 
@@ -101,7 +102,8 @@ def ParLine (lines):
         #print('len(str)=',len(str),str)
         if len(str) == 5 :
             price=float(str[1]) * 100
-            #print('price=',price)
+            t_idx=2*len(NUMBER)+3
+            #print('t_idx=',t_idx)
             dealNu=str[2]
             #print('num,price,Nu=',NUMBER,price,dealNu)
             NUMBER.append(dealNu)
@@ -111,22 +113,26 @@ def ParLine (lines):
             #print('num=',NUMBER,price,dealNu)
             #new_str=sorted(list(new_str.append(-1.5)),key=float)
             #print('priceIndex=',priceIndex,NUMBER)
-            B_index     =   2 * priceIndex + 1
-            S_index     =   2 * priceIndex + 2
-            BM_index =   2 * priceIndex + 3 + 2 * len(NUMBER)
-            SM_index =   2 * priceIndex + 4 + 2 * len(NUMBER)
+            B_index  =   2 * priceIndex + 1
+            S_index  =   2 * priceIndex + 2
+            BM_index =   2 * priceIndex + 3 + 1 + 2 * len(NUMBER)
+            SM_index =   2 * priceIndex + 4 + 1 + 2 * len(NUMBER)
             #print('bi,si,smi,smi,len(nub)=',B_index,S_index,BM_index,SM_index,len(NUMBER))
 
             if  re.search("B", line):
                 #print('B',len(str),str[1],str[2],str[3])
                 #print(str)
-                B_data       =                          float(dealNu) + float(COLUMN[B_index])
+                B_data    =                 float(dealNu) + float(COLUMN[B_index])
                 BM_data   =  float(price) * float(dealNu) + float(COLUMN[BM_index])
-                #print('B_index,BM_data',B_index,BM_data)
+                #print('B_data,BM_data',B_data,BM_data)
                 COLUMN[B_index]     = B_data
                 COLUMN[BM_index] = BM_data
                 ws.write(ROW,B_index,B_data) 
                 ws.write(ROW,BM_index,BM_data) 
+                #t_data    =  float(B_data) ;
+                #COLUMN[t_idx] = t_data
+                #print('t_idx,t_data',t_idx,t_data)
+                #ws.write(ROW,t_idx,t_data)
             elif re.search("S",line):
                 #print('S,S_index,col,len(col)',str,S_index,COLUMN[S_index],len(COLUMN))
                 #print('dealNu,S_index,SM_index,col(index)',dealNu,S_index,SM_index,COLUMN[S_index])
@@ -138,11 +144,16 @@ def ParLine (lines):
                 COLUMN[SM_index] = SM_data
                 ws.write(ROW,S_index,S_data) 
                 ws.write(ROW,SM_index,SM_data) 
+                #t_data    =  float(S_data) +  float(COLUMN[t_idx])
+                #COLUMN[t_idx] = t_data
+                #ws.write(ROW,t_idx,t_data)
             else :
-               pass
+                pass
         else :
             pass
-   
+    
+    
+    
 
     
     
@@ -158,13 +169,32 @@ def merge():
         fid = os.path.basename(i).split('.')[0]#带后缀的文件名
         ws.write(ROW,0,fid)
         #print('filename=',fid)
+        t_idx = 2*len(NUMBER)+3;
         for i in range(1,len(COLUMN)):
-            #ws.write(ROW,i,0) 
+            # if (i == t_idx ):
+                # print('t_idx',t_idx)
+                # #COLUMN[i] = 'SUM(B1:t_idx）'
+                # data='=SUM'+'('+'B'+str(str(ROW+1))+':'+chr(t_idx+64)+str(str(ROW+1))+')'
+                # print('data',data)
+                # ws.write(ROW,i,data) 
+            # else :
+                
+                #ws.write(ROW,i,0) 
             COLUMN[i]=0.0
 
         #print('number=aaa',NUMBER)
 
         ParLine(fopen.readlines())
+        #data = 0.0
+        #T_idx = int(t_idx) + 1
+        #print('idx',t_idx,T_idx)
+        for j in range(1,t_idx) :
+            #print('j',j)
+            #print(int(COLUMN[j]),COLUMN[t_idx])
+            COLUMN[t_idx] = int(COLUMN[j]) + int(COLUMN[t_idx])
+            #print('data',data)
+        #print('data',COLUMN[t_idx]);    
+        ws.write(ROW,t_idx,COLUMN[t_idx]) 
         ROW=ROW+1
 
     print('\n分析完成，生成文件为：',os.path.abspath(OFILE))  
